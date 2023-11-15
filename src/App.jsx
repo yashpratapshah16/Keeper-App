8f911ff7594e26909d26aab621284e4afa0e83fe
Yashpratap Shah <yashpratapshah16@gmail.com>
2023-11-15 21:03:17 +0530

    part 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import Header from "./components/Header"
-import Note from "./components/Note"
-import Footer from "./components/Footer"
-function App(){
-    return (<div>
-    <Header/>
-    <Note/>
-    <Footer/>
-    
-    </div>);
+import Header from "./components/Header";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+import notes from "./notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
